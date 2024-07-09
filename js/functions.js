a5da9f5e9e2dc4f41419a9e8ed521eca720cfb89
semenova-lv <lyudmila.klyueva@gmail.com>
2024-07-09 23:56:28 +0400

    5.16 функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -66,3 +66,28 @@ extractNumber(1.5); // 15
 // console.log(extractNumber(2023)); // 2023
 // console.log(extractNumber(-1));   // 1
 // console.log(extractNumber(1.5));  // 15
+
+// Делу — время
+// функция возвращает время в минутах
+const getTimeInMinute = (time) => {
+  const [hours, minutes] = time.split(':'); // получаем массив ['часы', 'минуты']
+  return Number(hours) * 60 + Number(minutes); // число минут относительно начала дня (0:00)
+};
+
+const getTimeWork = (timeDayStart, timeDayEnd, timeMeetingStart, durationMeeting) => {
+  const timeMeetingEnd = getTimeInMinute(timeMeetingStart) + durationMeeting;
+  return getTimeInMinute(timeMeetingStart) >= getTimeInMinute(timeDayStart) && timeMeetingEnd <= getTimeInMinute(timeDayEnd);
+};
+
+getTimeWork('08:00', '17:30', '14:00', 90);
+getTimeWork('8:0', '10:0', '8:0', 120);
+getTimeWork('08:00', '14:30', '14:00', 90);
+getTimeWork('14:00', '17:30', '08:0', 90);
+getTimeWork('8:00', '17:30', '08:00', 900);
+
+
+// console.log(getTimeWork('08:00', '17:30', '14:00', 90)); // true
+// console.log(getTimeWork('8:0', '10:0', '8:0', 120)); // true
+// console.log(getTimeWork('08:00', '14:30', '14:00', 90)); // false
+// console.log(getTimeWork('14:00', '17:30', '08:0', 90)); // false
+// console.log(getTimeWork('8:00', '17:30', '08:00', 900)); // false
