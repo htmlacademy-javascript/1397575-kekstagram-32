5ca518d87685d277747d5bc0011d8f6e03e4e33d
semenova-lv <lyudmila.klyueva@gmail.com>
2024-07-14 16:57:55 +0400

    отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {simularPhotoList} from './data.js';
+import {picturesBlock} from './thumbnails.js';
 
 // eslint-disable-next-line no-console
-console.table(simularPhotoList());
+console.log(picturesBlock);
