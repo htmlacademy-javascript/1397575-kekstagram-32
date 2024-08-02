9e7c999cee193f2b4944ecc24366a14a60d37c00
semenova-lv <lyudmila.klyueva@gmail.com>
2024-08-02 17:41:33 +0400

    Рефакторинг full-size-mode.js

--- js/full-size-mode.js
+++ js/full-size-mode.js
@@ -1,6 +1,5 @@
 import {isEscapeKey} from './util.js';
 
-const STEPVALUE = 5;
 const thumbnails = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const buttonClosePictire = bigPicture.querySelector('.big-picture__cancel');
@@ -9,62 +8,66 @@ const commentItem = commentsList.querySelector('.social__comment');
 const counterComments = bigPicture.querySelector('.social__comment-count');
 const loaderComments = bigPicture.querySelector('.comments-loader');
 const shownCountComments = counterComments.querySelector('.social__comment-shown-count');
-
-//функция для поочередного вывода списка комментариев
-let countStep = 1;
-const createCounterComments = () => {
-  const commentsArray = commentsList.querySelectorAll('.social__comment');
-  commentsArray.forEach((comment) => comment.classList.add('hidden'));
-  loaderComments.classList.remove('hidden');
-  counterComments.classList.remove('hidden');
-  if (commentsArray.length <= STEPVALUE * countStep) {
-    shownCountComments.textContent = commentsArray.length;
-    commentsArray.forEach((comment) => comment.classList.remove('hidden'));
-    loaderComments.classList.add('hidden');
-  } else {
-    shownCountComments.textContent = STEPVALUE * countStep;
-    for (let i = 0; i < STEPVALUE * countStep; i++) {
-      commentsArray[i].classList.remove('hidden');
-    }
-  }
-};
-
-const onButtonLoaderClick = () => {
-  countStep++;
-  createCounterComments();
-};
+const commentsListFragment = document.createDocumentFragment(); //для добавления комментариев
+let showComments = 0;
+let commentsArray = [];
 
 // Заполнение поста данными
-const createFullSizePicture = ({url, description, likes, comments}) => {
+const createFullSizePicture = ({url, description, likes}) => {
   bigPicture.querySelector('img').src = url;
   bigPicture.querySelector('img').alt = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.social__caption').textContent = description;
-  //для счетчика комментариев
-  counterComments.querySelector('.social__comment-total-count').textContent = comments.length;
+};
 
+//функция для отрисовки каждого комментария
+const createComment = ({avatar, name, message}) => {
+  const comment = commentItem.cloneNode(true);
+
+  comment.querySelector('img').src = avatar;
+  comment.querySelector('img').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+// функция для генерации комментариев
+const renderComments = (comments) => {
+  showComments += 5;
+
+  if (showComments >= comments.length) {
+    loaderComments.classList.add('hidden');
+    showComments = comments.length;
+  } else {
+    loaderComments.classList.remove('hidden');
+  }
+
+  for (let i = 0; i < showComments; i++) {
+    const item = createComment(comments[i]);
+    commentsListFragment.append(item);
+  }
   commentsList.innerHTML = '';
+  commentsList.append(commentsListFragment);
+  counterComments.querySelector('.social__comment-total-count').textContent = comments.length;
+  shownCountComments.textContent = showComments;
+};
 
-  comments.forEach(({avatar, name, message}) => {
-    const comment = commentItem.cloneNode(true);
-
-    comment.querySelector('img').src = avatar;
-    comment.querySelector('img').alt = name;
-    comment.querySelector('.social__text').textContent = message;
-    commentsList.append(comment);
-  });
-  createCounterComments();
+const onButtonLoaderClick = () => {
+  renderComments(commentsArray);
 };
 
 // Функция для отображения поста с изображением
-const openFullSizePost = () => {
-  countStep = 1; // переменная для работы прокрутки комментариев
+const openFullSizePost = (data) => {
   bigPicture.classList.remove('hidden');
-  counterComments.classList.add('hidden');
   loaderComments.classList.add('hidden');
   document.body.classList.add('modal-open');
 
+  createFullSizePicture(data);
+  commentsArray = data.comments;
+  renderComments(commentsArray);
+
   document.addEventListener('keydown', onDocumentKeydown);
+  buttonClosePictire.addEventListener('click',closeFullSizePost);
   loaderComments.addEventListener('click', onButtonLoaderClick);
 };
 
@@ -72,7 +75,10 @@ const openFullSizePost = () => {
 function closeFullSizePost() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
+
+  showComments = 0;
   document.removeEventListener('keydown', onDocumentKeydown);
+  buttonClosePictire.removeEventListener('click',closeFullSizePost);
   loaderComments.removeEventListener('click', onButtonLoaderClick);
 }
 
@@ -83,19 +89,17 @@ function onDocumentKeydown(evt) {
   }
 }
 
-const generateFullSizePost = (array) => {
+const generateFullSizePost = (data) => {
   const onThumbnailClick = (evt) => {
-    if (evt.target.closest('a')) {
+    const currentThumbnail = evt.target.closest('.picture');
+    if (currentThumbnail) {
       evt.preventDefault();
-      openFullSizePost();
-      bigPicture.classList.remove('hidden');
-      const id = Number(evt.target.closest('a').dataset.id);
-      createFullSizePicture(array[array.findIndex((elem) => elem.id === id)]);
+      const id = Number(currentThumbnail.dataset.id);
+      openFullSizePost(data[data.findIndex((elem) => elem.id === id)]);
     }
   };
 
   thumbnails.addEventListener('click', onThumbnailClick);
-  buttonClosePictire.addEventListener('click',closeFullSizePost);
 };
 
 export {generateFullSizePost};
